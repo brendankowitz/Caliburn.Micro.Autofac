e7221c15038e53f7897a8477d814617d08d7ac0a
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-11-10 17:33:39 +0100

    style(wpf): format spacing

--- src/Caliburn.Micro.Autofac.WPF/EventAggregationAutoSubscriptionModule.cs
+++ src/Caliburn.Micro.Autofac.WPF/EventAggregationAutoSubscriptionModule.cs
@@ -3,45 +3,43 @@ using Autofac.Core;
 
 namespace Caliburn.Micro.Autofac
 {
-  public class EventAggregationAutoSubscriptionModule : Module
-  {
-    protected override void AttachToComponentRegistration(IComponentRegistry registry,
-                                                          IComponentRegistration registration)
+    public class EventAggregationAutoSubscriptionModule : Module
     {
-      registration.Activated += (sender,
-                                 args) =>
-                                {
-                                  //  we never want to fail, so check for null (should never happen), and return if it is
-                                  if (args == null)
-                                  {
-                                    return;
-                                  }
+        protected override void AttachToComponentRegistration(IComponentRegistry registry, IComponentRegistration registration)
+        {
+            registration.Activated += (sender, args) =>
+            {
+                //  we never want to fail, so check for null (should never happen), and return if it is
+                if (args == null)
+                {
+                    return;
+                }
 
-                                  //  try to convert instance to IHandle
-                                  //  I originally did e.Instance.GetType().IsAssignableTo<>() and then 'as',
-                                  //  but it seemed redundant
+                //  try to convert instance to IHandle
+                //  I originally did e.Instance.GetType().IsAssignableTo<>() and then 'as',
+                //  but it seemed redundant
 
-                                  var handler = args.Instance as IHandle;
-                                  if (handler == null)
-                                  {
-                                    return;
-                                  }
+                var handler = args.Instance as IHandle;
+                if (handler == null)
+                {
+                    return;
+                }
 
-                                  //  if it is not null, it implements, so subscribe
+                //  if it is not null, it implements, so subscribe
 
-                                  var context = args.Context;
-                                  var lifetimeScope = context.Resolve<ILifetimeScope>();
-                                  var eventAggregator = lifetimeScope.Resolve<IEventAggregator>();
+                var context = args.Context;
+                var lifetimeScope = context.Resolve<ILifetimeScope>();
+                var eventAggregator = lifetimeScope.Resolve<IEventAggregator>();
 
-                                  eventAggregator.Subscribe(handler);
+                eventAggregator.Subscribe(handler);
 
-                                  var disposableWrapper = new DisposableWrapper(() =>
-                                                                                {
-                                                                                  eventAggregator.Unsubscribe(handler);
-                                                                                });
+                var disposableWrapper = new DisposableWrapper(() =>
+                {
+                    eventAggregator.Unsubscribe(handler);
+                });
 
-                                  lifetimeScope.Disposer.AddInstanceForDisposal(disposableWrapper);
-                                };
+                lifetimeScope.Disposer.AddInstanceForDisposal(disposableWrapper);
+            };
+        }
     }
-  }
 }
